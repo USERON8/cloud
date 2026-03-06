a675ad8237e189383e71113787761d82688f94a6
USERON8 <sltt_0212@qq.com>
2026-03-06 22:36:45 +0800

    refactor(user): share account upsert fields

--- common-parent/common-domain/src/main/java/com/cloud/common/domain/dto/user/MerchantUpsertRequestDTO.java
+++ common-parent/common-domain/src/main/java/com/cloud/common/domain/dto/user/MerchantUpsertRequestDTO.java
@@ -1,26 +1,15 @@
 package com.cloud.common.domain.dto.user;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.io.Serial;
-import java.io.Serializable;
-
-@Data
-public class MerchantUpsertRequestDTO implements Serializable {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
-
-    @Size(max = 50, message = "username length must be less than or equal to 50")
-    private String username;
-
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Size(max = 255, message = "password length must be less than or equal to 255")
-    private String password;
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+public class MerchantUpsertRequestDTO extends BaseAccountUpsertRequestDTO {
 
     @Size(max = 100, message = "merchantName length must be less than or equal to 100")
     private String merchantName;
@@ -28,9 +17,4 @@ public class MerchantUpsertRequestDTO implements Serializable {
     @Email(message = "invalid email format")
     @Size(max = 100, message = "email length must be less than or equal to 100")
     private String email;
-
-    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "invalid phone format")
-    private String phone;
-
-    private Integer status;
 }
