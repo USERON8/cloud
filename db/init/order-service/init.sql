b48becdde7dc2aa2d6c3ca21daadcfdc9c325e90
USERON8 <sltt_0212@qq.com>
2026-03-08 15:35:22 +0800

    Refactor code structure for improved readability and maintainability

--- db/init/order-service/init.sql
+++ db/init/order-service/init.sql
@@ -253,3 +253,14 @@ CREATE TABLE IF NOT EXISTS inbox_consume_log
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
+  
+CREATE TABLE undo_log (
+  branch_id BIGINT NOT NULL,
+  xid VARCHAR(128) NOT NULL,
+  context VARCHAR(128) NOT NULL,
+  rollback_info LONGBLOB NOT NULL,
+  log_status INT NOT NULL,
+  log_created DATETIME NOT NULL,
+  log_modified DATETIME NOT NULL,
+  UNIQUE KEY ux_undo_log (xid,branch_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
