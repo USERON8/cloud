8433ab1697b011b69777fa39979e88e4680abb44
USERON8 <sltt_0212@qq.com>
2026-03-12 17:22:24 +0800

    feat: add motion animations to marketplace

--- my-shop-web/src/main.ts
+++ my-shop-web/src/main.ts
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 import VueLazyload from 'vue-lazyload'
+import { MotionPlugin } from '@vueuse/motion'
 
 const app = createApp(App)
 app.use(router)
 app.use(VueLazyload, {
   observer: true
 })
+app.use(MotionPlugin)
 app.mount('#app')
