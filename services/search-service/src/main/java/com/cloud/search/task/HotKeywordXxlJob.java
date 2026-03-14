4921103b773a6b1a5d8638b7825aefbe8c25eb18
USERON8 <sltt_0212@qq.com>
2026-03-15 01:28:32 +0800

    feat(search): add hot keyword persist xxl job

---
+++ services/search-service/src/main/java/com/cloud/search/task/HotKeywordXxlJob.java
@@ -0,0 +1,39 @@
+package com.cloud.search.task;
+
+import com.cloud.common.annotation.DistributedLock;
+import com.cloud.search.service.support.HotKeywordSyncService;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HotKeywordXxlJob {
+
+    private final HotKeywordSyncService hotKeywordSyncService;
+
+    @XxlJob("hotKeywordPersistJob")
+    @DistributedLock(
+            key = "'xxl:search:hot-keyword-persist'",
+            waitTime = 1,
+            leaseTime = 300,
+            failStrategy = DistributedLock.LockFailStrategy.RETURN_NULL
+    )
+    public void persistHotKeywords() {
+        try {
+            hotKeywordSyncService.syncToDb();
+            logMessage("hotKeywordPersistJob finished");
+        } catch (Exception ex) {
+            log.error("Hot keyword persist job failed", ex);
+            XxlJobHelper.handleFail(ex.getMessage());
+        }
+    }
+
+    private void logMessage(String message) {
+        XxlJobHelper.log(message);
+        log.info(message);
+    }
+}
