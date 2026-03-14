25884f2f92640a65e3b2437cbf03bbee166d4f04
USERON8 <sltt_0212@qq.com>
2026-03-14 19:08:25 +0800

    feat: add global JWT security scheme to OpenAPI

--- common-parent/common-web/src/main/java/com/cloud/common/config/BaseKnife4jConfig.java
+++ common-parent/common-web/src/main/java/com/cloud/common/config/BaseKnife4jConfig.java
@@ -1,10 +1,13 @@
 package com.cloud.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +19,17 @@ import java.util.List;
 public abstract class BaseKnife4jConfig {
 
     protected OpenAPI createOpenAPI(String title, String description, String version) {
+        SecurityScheme jwtScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .description("JWT Bearer Token");
+
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Authorization", jwtScheme))
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
                 .info(new Info()
                         .title(title)
                         .description(description)
