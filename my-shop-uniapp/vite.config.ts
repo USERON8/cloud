998a071c97dfb0c700003341a8042166221f2136
USERON8 <sltt_0212@qq.com>
2026-03-15 21:26:34 +0800

    fix(frontend): load uni plugin default export

--- my-shop-uniapp/vite.config.ts
+++ my-shop-uniapp/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
+import { default as uni } from '@dcloudio/vite-plugin-uni'
 import path from 'node:path'
 
 export default defineConfig({
