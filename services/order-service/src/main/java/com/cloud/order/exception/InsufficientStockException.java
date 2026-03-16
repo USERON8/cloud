0bd9f6e94f4b09b7330a95be431ff4bac2378c88
USERON8 <sltt_0212@qq.com>
2026-03-16 21:49:04 +0800

    refactor: remove service-specific exception handlers

--- services/order-service/src/main/java/com/cloud/order/exception/InsufficientStockException.java
+++ services/order-service/src/main/java/com/cloud/order/exception/InsufficientStockException.java
@@ -1,39 +1,38 @@
 package com.cloud.order.exception;
 
-import com.cloud.common.exception.BusinessException;
+import com.cloud.common.enums.ResultCode;
+import com.cloud.common.exception.BizException;
 
-public class InsufficientStockException extends BusinessException {
+public class InsufficientStockException extends BizException {
 
-  private static final int INSUFFICIENT_STOCK_ERROR_CODE = 40002;
+    private Long productId;
 
-  private Long productId;
+    public InsufficientStockException(String message) {
+        super(ResultCode.STOCK_INSUFFICIENT, message);
+    }
 
-  public InsufficientStockException(String message) {
-    super(INSUFFICIENT_STOCK_ERROR_CODE, message);
-  }
+    public InsufficientStockException(Long productId, Integer required, Integer available) {
+        super(
+                ResultCode.STOCK_INSUFFICIENT,
+                String.format("库存不足，商品ID: %d，需要: %d，可用: %d", productId, required, available));
+        this.productId = productId;
+    }
 
-  public InsufficientStockException(Long productId, Integer required, Integer available) {
-    super(
-        INSUFFICIENT_STOCK_ERROR_CODE,
-        String.format("鍟嗗搧[ID:%d]搴撳瓨涓嶈冻锛岄渶瑕侊細%d锛屽彲鐢細%d", productId, required, available));
-    this.productId = productId;
-  }
+    public InsufficientStockException(String productName, Integer required, Integer available) {
+        super(
+                ResultCode.STOCK_INSUFFICIENT,
+                String.format("库存不足，商品: %s，需要: %d，可用: %d", productName, required, available));
+    }
 
-  public InsufficientStockException(String productName, Integer required, Integer available) {
-    super(
-        INSUFFICIENT_STOCK_ERROR_CODE,
-        String.format("鍟嗗搧[%s]搴撳瓨涓嶈冻锛岄渶瑕侊細%d锛屽彲鐢細%d", productName, required, available));
-  }
+    public InsufficientStockException(String message, Throwable cause) {
+        super(ResultCode.STOCK_INSUFFICIENT, message, cause);
+    }
 
-  public InsufficientStockException(String message, Throwable cause) {
-    super(INSUFFICIENT_STOCK_ERROR_CODE, message, cause);
-  }
+    public Long getProductId() {
+        return productId;
+    }
 
-  public Long getProductId() {
-    return productId;
-  }
-
-  public void setProductId(Long productId) {
-    this.productId = productId;
-  }
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
 }
--- services/order-service/src/main/java/com/cloud/order/exception/OrderBusinessException.java
+++ services/order-service/src/main/java/com/cloud/order/exception/OrderBusinessException.java
@@ -1,58 +1,59 @@
 package com.cloud.order.exception;
 
-public class OrderBusinessException extends RuntimeException {
+import com.cloud.common.enums.ResultCode;
+import com.cloud.common.exception.BizException;
 
-  private final Long orderId;
+public class OrderBusinessException extends BizException {
 
-  private final String errorCode;
+    private final Long orderId;
+    private final String errorCode;
 
-  public OrderBusinessException(String message) {
-    super(message);
-    this.orderId = null;
-    this.errorCode = null;
-  }
-
-  public OrderBusinessException(String message, Throwable cause) {
-    super(message, cause);
-    this.orderId = null;
-    this.errorCode = null;
-  }
-
-  public OrderBusinessException(Long orderId, String message) {
-    super(message);
-    this.orderId = orderId;
-    this.errorCode = null;
-  }
-
-  public OrderBusinessException(Long orderId, String errorCode, String message) {
-    super(message);
-    this.orderId = orderId;
-    this.errorCode = errorCode;
-  }
-
-  public OrderBusinessException(Long orderId, String message, Throwable cause) {
-    super(message, cause);
-    this.orderId = orderId;
-    this.errorCode = null;
-  }
-
-  public Long getOrderId() {
-    return orderId;
-  }
-
-  public String getErrorCode() {
-    return errorCode;
-  }
-
-  public int getCode() {
-
-    if (errorCode != null) {
-      try {
-        return Integer.parseInt(errorCode);
-      } catch (NumberFormatException e) {
-        return 500;
-      }
+    public OrderBusinessException(String message) {
+        super(ResultCode.BUSINESS_ERROR, message);
+        this.orderId = null;
+        this.errorCode = null;
     }
-    return 500;
-  }
+
+    public OrderBusinessException(String message, Throwable cause) {
+        super(ResultCode.BUSINESS_ERROR, message, cause);
+        this.orderId = null;
+        this.errorCode = null;
+    }
+
+    public OrderBusinessException(Long orderId, String message) {
+        super(ResultCode.BUSINESS_ERROR, message);
+        this.orderId = orderId;
+        this.errorCode = null;
+    }
+
+    public OrderBusinessException(Long orderId, String errorCode, String message) {
+        super(resolveCode(errorCode), message);
+        this.orderId = orderId;
+        this.errorCode = errorCode;
+    }
+
+    public OrderBusinessException(Long orderId, String message, Throwable cause) {
+        super(ResultCode.BUSINESS_ERROR, message, cause);
+        this.orderId = orderId;
+        this.errorCode = null;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    private static int resolveCode(String errorCode) {
+        if (errorCode == null || errorCode.isBlank()) {
+            return ResultCode.BUSINESS_ERROR.getCode();
+        }
+        try {
+            return Integer.parseInt(errorCode);
+        } catch (NumberFormatException ex) {
+            return ResultCode.BUSINESS_ERROR.getCode();
+        }
+    }
 }
