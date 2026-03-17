9540336e2dd65f69ae178288a2f5c70d3e13e09d
USERON8 <sltt_0212@qq.com>
2026-03-17 16:42:53 +0800

    fix: harden dead letter and outbox wiring

--- common-parent/common-messaging/src/main/java/com/cloud/common/messaging/config/DeadLetterAutoConfiguration.java
+++ common-parent/common-messaging/src/main/java/com/cloud/common/messaging/config/DeadLetterAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.cloud.common.messaging.config;
 import com.cloud.common.messaging.deadletter.DeadLetterService;
 import com.cloud.common.messaging.deadletter.JdbcDeadLetterService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -12,14 +13,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @AutoConfiguration
-@ConditionalOnClass(JdbcTemplate.class)
+@ConditionalOnClass({JdbcTemplate.class, DataSource.class})
 public class DeadLetterAutoConfiguration {
 
   @Bean
-  @ConditionalOnBean(JdbcTemplate.class)
+  @ConditionalOnBean(DataSource.class)
   @ConditionalOnMissingBean(DeadLetterService.class)
   public DeadLetterService deadLetterService(
-      JdbcTemplate jdbcTemplate, ObjectProvider<ObjectMapper> mapperProvider) {
-    return new JdbcDeadLetterService(jdbcTemplate, mapperProvider);
+      DataSource dataSource, ObjectProvider<ObjectMapper> mapperProvider) {
+    return new JdbcDeadLetterService(new JdbcTemplate(dataSource), mapperProvider);
   }
 }
