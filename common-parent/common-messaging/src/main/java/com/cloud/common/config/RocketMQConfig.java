c95a1e989cc8c912cf1af358cdcb13f6bc711430
USERON8 <sltt_0212@qq.com>
2026-04-09 08:43:18 +0800

    chore: drop unused rocketmq health bean

--- common-parent/common-messaging/src/main/java/com/cloud/common/config/RocketMQConfig.java
+++ common-parent/common-messaging/src/main/java/com/cloud/common/config/RocketMQConfig.java
@@ -2,26 +2,9 @@ package com.cloud.common.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
 @ConditionalOnProperty(name = "spring.cloud.stream.rocketmq.binder.name-server")
-public class RocketMQConfig {
-
-  @Bean
-  public RocketMQHealthIndicator rocketMQHealthIndicator() {
-    return new RocketMQHealthIndicator();
-  }
-
-  public static class RocketMQHealthIndicator {
-
-    public RocketMQHealthIndicator() {}
-
-    public boolean isHealthy() {
-
-      return true;
-    }
-  }
-}
+public class RocketMQConfig {}
