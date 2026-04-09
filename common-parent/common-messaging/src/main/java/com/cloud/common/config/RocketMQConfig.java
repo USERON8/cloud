5a3b443d9eb15892e479854660296b001e6fd24a
USERON8 <sltt_0212@qq.com>
2026-04-09 08:46:52 +0800

    chore: remove unused rocketmq config logger

--- common-parent/common-messaging/src/main/java/com/cloud/common/config/RocketMQConfig.java
+++ common-parent/common-messaging/src/main/java/com/cloud/common/config/RocketMQConfig.java
@@ -1,10 +1,8 @@
 package com.cloud.common.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 @ConditionalOnProperty(name = "spring.cloud.stream.rocketmq.binder.name-server")
 public class RocketMQConfig {}
